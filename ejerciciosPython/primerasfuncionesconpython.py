55fd16ad49549014576f87025f26a18d2392216d
Alexis Portillo <Alexis-Portillo@kuatiapo.com.py>
2022-05-13 16:38:34 -0400

    Ejercicios de primeras funciones

---
+++ ejerciciosPython/primerasfuncionesconpython.py
@@ -0,0 +1,36 @@
+# MULTIPLICACION
+
+def multiplicar(numeroUno,numeroDos):
+
+	return numeroUno * numeroDos
+
+
+print(multiplicar(20,5))
+
+# SUMAR
+
+def sumar(numero1,numero2):
+
+	return numero1 + numero2
+
+print(sumar(1050,50))
+
+
+
+# RESTAR
+
+def restar(numero1,numero2):
+
+	return numero1 - numero2
+
+print(restar(100,5))
+
+
+#DIVISION
+
+def dividir(numeroUno,numeroDos):
+
+	return numeroUno - numeroDos
+
+print(dividir(25,5))
+
